e7b848907722d7be9024e20b19558ee66c65f2e1
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-03-10 00:02:04 +0900

    weather apiの実行まで実装

--- react-weather-app/src/components/Form.js
+++ react-weather-app/src/components/Form.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
     const [city, setCity] = useState("");
+    const getWeather = (e) => {
+        e.preventDefault();
+        axios.get("https://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=London&aqi=no")
+        .then(res => console.log(res));
+    };
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange = {e => setCity(e.target.value)} />
-            {city}
-            <button type="submit">Get Wwather</button>
+            <button type="submit" onClick={getWeather} >Get Wwather</button>
         </form>
     );
 };
