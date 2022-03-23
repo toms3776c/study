afeeb135c8fbeeb1797f0dc20b392a68abb814be
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-03-24 00:12:09 +0900

    Form.tsxからApp.tsxへ処理移動。

--- react-typescript-weather-app/src/App.tsx
+++ react-typescript-weather-app/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+      e.preventDefault();
+      fetch("https://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <div className="App">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 }
--- react-typescript-weather-app/src/components/Form.tsx
+++ react-typescript-weather-app/src/components/Form.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react';
+type FormPropsType = {
+    setCity: React.Dispatch<React.SetStateAction<string>>;
+    getWeather: (e: any) => void;
+}
 
-const Form = () => {
-    const [city, setCity] = useState<string>("");
-    const getWeather = (e: any) => {
-        e.preventDefault();
-        fetch("https://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=London&aqi=no")
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
+const Form = (props: FormPropsType) => {
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
             
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
