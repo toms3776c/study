65a044af047b49eaa735aecd05fd6ed85759b25b
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-03-24 23:38:01 +0900

    APIの検索結果を画面表示するまで。

--- react-typescript-weather-app/src/App.tsx
+++ react-typescript-weather-app/src/App.tsx
@@ -4,19 +4,42 @@ import Form from './components/Form';
 import Results from './components/Results';
 import './App.css';
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
+
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (e: any) => {
       e.preventDefault();
       fetch("https://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=London&aqi=no")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        })
+      })
   }
   return (
     <div className="App">
       <Title />
       <Form setCity={setCity} getWeather={getWeather} />
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
--- react-typescript-weather-app/src/components/Results.tsx
+++ react-typescript-weather-app/src/components/Results.tsx
@@ -1,6 +1,26 @@
-const Results = () => {
+type ResultsPropcType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperature: string;
+        conditionText: string;
+        icon: string;
+    }
+}
+
+const Results = (props: ResultsPropcType) => {
     return (
-        <h1>気象データ</h1>
+        <div>
+            {props.results.cityName && <div>{props.results.cityName}</div>}
+            {props.results.country && <div>{props.results.country}</div>}
+            {props.results.temperature && <div>{props.results.temperature} <span>℃</span></div>}
+            {props.results.conditionText && 
+                <div>
+                    <img src={props.results.icon} alt="icon" />
+                    {props.results.conditionText}
+                </div>
+            }
+        </div>
     );
 }
 
